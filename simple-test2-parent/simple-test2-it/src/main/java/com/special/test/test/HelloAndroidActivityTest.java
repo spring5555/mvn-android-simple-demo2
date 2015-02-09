98d12f1ad36eedd43e8d2fa2c0d4ec8cc818c4ec
fengyunhui <yh.feng@1tianxia.net>
2015-02-09 10:26:47 +0800

    初始化项目

---
+++ simple-test2-parent/simple-test2-it/src/main/java/com/special/test/test/HelloAndroidActivityTest.java
@@ -0,0 +1,17 @@
+package com.special.test.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+import com.special.test.*;
+
+public class HelloAndroidActivityTest extends ActivityInstrumentationTestCase2<HelloAndroidActivity> {
+
+    public HelloAndroidActivityTest() {
+        super(HelloAndroidActivity.class); 
+    }
+
+    public void testActivity() {
+        HelloAndroidActivity activity = getActivity();
+        assertNotNull(activity);
+    }
+}
+
